fc7c4bd97a220ee285f874d0f44e894e0f16fb47
Juan John-Charles <whoisju1@gmail.com>
2017-02-27 20:20:08 -0500

    App redone, mostly complete but still missing some things

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,161 +1,131 @@
-// This is a function to set the questions and options on the page
-function contentSetter(item) {
-  $("#question").html(item[0]);
-  $("#answer1").html(item[1]);
-  $("#answer2").html(item[2]);
-  $("#answer3").html(item[3]);
-  $("#answer4").html(item[4]);
-  $("#correct-ans").html(item[5]);
-}
+/*************************TRIVIAL TRIVIA*******************************/
+/***************PSEUDO CODE*************************/
 
-var counter = 0;
+//MAKE AN ARRAT OF OJECTS
+//EACH OBJECT REPRESENTS A PAGE WITH QUESTIONS AND THE PROSPECTIVE ANSWERS
 
-function startScreen() {
-  $(".q-and-a").css("display", "none");
-  $("#question-area").append("<div id='start-screen'></div>");
-  $("#start-screen").html("<button id='start-btn'>Click Here to Start</button>");
-  $("#start-btn").on('click', function() {
-    $("#start-screen").css("display", "none");
-    contentSetter(item[counter].newContent());
-    $(".q-and-a").css("display", "block");
-	  console.log("counter: " + counter + ", item.length: " + item.length);
-   if (counter <= item.length) {
-	   countdown(15, printTime, nextQuestion);
-  if (counter === item.length) {
-    return finalScreen();
-  }
-   } 
-  function printTime() {
-    $("#countdown").html(secondsLeft);
-  }
- 
-  contentSetter(item[counter].newContent()); //the content setter function is called, placing the Q&A on the page
-  });
-}
+//CREATE A FUNCTION THAT PRINTS THE OJECTS TO THE THE PAGE, WITH EACH PROPERTY PRIINTED TO ITS RESPECTIVE SPOT ON THE PAGE
+//THERE SHOULD BE A VARIABLE THAT SPECIFIES THE OBJECT IS BEING PRINTED
 
-startScreen();
 
-function finalScreen() {
-  $(".q-and-a").css("display", "none");
-  $("#question").hide();
-}
-
-var secondsLeft;
-
-function countdown(timeLeft, doWhile, doAtEnd) {
-	
-  secondsLeft = 0;
-	secondsLeft = timeLeft;
-  clearInterval(timer);
-  var timer = setInterval(function() {
-    secondsLeft--;
-    doWhile();
-    console.log("secondsLeft: " + secondsLeft);
-    if (secondsLeft === 0) {
-      doAtEnd();
-      clearInterval(timer);
-    }
-  }, 1000);
-}
-
-// This is an array of objects containing the questions and answers for the trivia, with the method creating an array values that will be entered as arguments in the contentSetter function
-var item = [{
-    "question": "Which territory is not a part of the Caribbean?",
-    "options": ["Dominica", "Jamacia", "St. Martin", "Hawaii"],
-    "rightAnswer": "Hawaii",
-    "newContent": function() {
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
-    }
-  },
-
-  {
-    "question": "The shortest runway on earth, not more than 1,300 feet long, can be found on which Caribbean island?",
-    "options": ["Barbabos", "Jamacia", "Saba", "Antigua"],
-    "rightAnswer": "Saba",
-    "newContent": function() {
-		"use strict";
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
-    }
-  }, {
-    "question": "Which island is known as the Nature Isle of the Caribbean?",
-    "options": ["Dominia", "Puerto Rico", "St. Martin", "Grenada"],
-    "rightAnswer": "Dominia",
-    "newContent": function() {
-		"use strict";
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
+//-------------AND ARRAY OF OBJECTS CONTAINING STRINGS (THE QUESTIONS, CHOICES, AND ANSWERS)-----------
+var items = [
+    {
+        "question": "Which territory is not a part of the Caribbean?",
+        "choices": ["Dominica", "Jamacia", "St. Martin", "Hawaii"],
+        "rightAnswer": "Hawaii"
     },
-  },
 
-  {
-    "question": "The only foreign place that former president George Washington ever visited was...",
-    "options": ["Dominican Republic", "Jamacia", "Cuba", "Barbados"],
-    "rightAnswer": "Barbados",
-    "newContent": function() {
-		"use strict";
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
+    {
+        "question": "The shortest runway on earth, not more than 1,300 feet long, can be found on which Caribbean island?",
+        "choices": ["Barbabos", "Jamacia", "Saba", "Antigua"],
+        "rightAnswer": "Saba"
     }
-  },
+    , {
+        "question": "Which island is known as the Nature Isle of the Caribbean?",
+        "choices": ["Dominia", "Puerto Rico", "St. Martin", "Grenada"],
+        "rightAnswer": "Dominia"
+    },
 
-  {
-    "question": "The very first James Bond film titled Dr. No was filmed in the Caribbean Island of... ",
-    "options": ["Trinidad & Tobago", "Jamacia", "St. Martin", "Guadeloupe"],
-    "rightAnswer": "Hawaii",
-    "newContent": function() {
-		"use strict";
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
-    }
-  },
+    {
+        "question": "The only foreign place that former president George Washington ever visited was...",
+        "choices": ["Dominican Republic", "Jamacia", "Cuba", "Barbados"],
+        "rightAnswer": "Barbados"
+    },
 
-  {
-    "question": "How many countries are there in the Caribbean?",
-    "options": ["100", "50", "75", "25"],
-    "rightAnswer": "Hawaii",
-    "newContent": function() {
-		"use strict";
-      $("h1").append("<div id='finalpage'>");
-      return [this.question, this.options[0], this.options[1], this.options[2], this.options[3], this.rightAnswer];
+    {
+        "question": "The very first James Bond film titled Dr. No was filmed in the Caribbean Island of... ",
+        "choices": ["Trinidad & Tobago", "Jamacia", "St. Martin", "Guadeloupe"],
+        "rightAnswer": "Hawaii"
+    },
+
+    {
+        "question": "How many countries are there in the Caribbean?",
+        "choices": ["100", "50", "75", "25"],
+        "rightAnswer": "Hawaii"
     }
-  }
 ];
 
-function nextQuestion(strtPg) { //press the submit button to get the questions & answers on the page
-  counter += 1; //this counter travases the array of objects (that contain the Q&A). 
-  console.log("counter: " + counter + ", item.length: " + item.length);
-    var secondsLeft = 15;
-   if (counter <= item.length) {
-	clearInterval(timer);
-	var timer = setInterval(function() {
-    secondsLeft--;
-    printTime();
-    clearInterval(timer);
-    console.log("secondsLeft: " + secondsLeft);
-    if (secondsLeft === 0) {
-      clearInterval(timer);
-      nextQuestion();	
-    }
-  }, 1000);
-  if (counter === item.length) {
-    return finalScreen();
-  }
-   } 
-  function printTime() {
-    $("#countdown").html(secondsLeft);
-  }
- 
-  contentSetter(item[counter].newContent()); //the content setter function is called, placing the Q&A on the page
+var count = -1,
+    timeRemaining;
+
+
+//----------FUNCTION TO PRINT CONTENT ON THE PAGE--------------
+function contentSetter() {
+    //print question 
+    $("#question").text((count+1)+". "+items[count].question);
+
+    //print choices
+
+    $("#ans1").html("<span class='items-identifier'>A.</span> " + items[count].choices[0]);
+    $("#ans2").html("<span class='items-identifier'>B.</span> " + items[count].choices[1]);
+    $("#ans3").html("<span class='items-identifier'>C.</span> " + items[count].choices[2]);
+    $("#ans4").html("<span class='items-identifier'>D.</span> " + items[count].choices[3]);
+    $(".items-identifier").css({
+        fontWeight: "bold"
+    });
+
+
+    //print Correct answer 
+    $("#answer").html("<span id='items-identifier'>THE ANSWER IS: </span>"+ items[count].rightAnswer);
 }
 
-var btnclick = $(".submit-button").on('click',nextQuestion);
+//------------------CODE FOR START BUTTON----------------------
 
-var interimPage = $("#question").append("<div id='interimPage'></div>"); //interim page created
-$("#interimPage").html("<div><p id='x-or-tick'></p></div><div id='correct-ans></div>"); // divs inside of interim-page created
+var startBtn = $("#start-button").click(function () {
 
-function lunchInterim() {
-  $(".q-and-a").hide();
-  $("#interimPage").show();
-  if ("TODO: IF ANSWER IS CORRECT") {
-    $("#x-or-tick").html("<img src='assets/images/tick.pgn'");
-  } else {
-    $("xOrTick").html("<img src='assets/images/tick.x'");
-  }
+    //change content displayed
+    $("#timer").show();
+    $("#start-page").hide();
+    QuesAndAns();
+})
+
+//-----------CODE FOR SUMBIT BUTTON-------------
+
+var submitBtn = $("#submit-btn").click(function () {
+    timeRemaining = 0;
+})
+
+//-------CODE FOR FUNCTION THAT PRINTS TRIVIA CONTENT----------
+
+function QuesAndAns() {
+    //change content dislayed
+    $("#answer-display-page").hide();
+    $("#trivia-content").show();
+    count++;
+    contentSetter();
+    console.log("count: " + count);
+    timeRemaining = 15;
+    var timer = setInterval(function () {
+        timeRemaining--;
+        console.log("timeRemaining :" + timeRemaining);
+        $("#countdown").text(timeRemaining);
+        if (timeRemaining === 0) {
+            clearInterval(timer);
+            answerDisplay();
+        }
+    }, 1000);
+}
+
+//-------CODE FOR FUNCTION THAT DISPLAYS ANSWER-------------
+
+function answerDisplay() {
+    //change content displayed
+    
+    $("#trivia-content").hide();
+    $("#answer-display-page").show();
+
+    timeRemaining = 0;
+    timeRemaining = 10;
+    var timer = setInterval(function () {
+        timeRemaining--;
+        $("#countdown").text(timeRemaining);
+        console.log("timeRemaining :" + timeRemaining);
+        if (timeRemaining === 0) {
+            // Timer stops if time runs out or if sumbit is pressed
+            clearInterval(timer);
+            QuesAndAns();
+        }
+    }, 1000);
+
 }
