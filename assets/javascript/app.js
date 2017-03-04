c478212f207be624633d35f6ed90e40e826b6018
Juan John-Charles <whoisju1@gmail.com>
2017-03-04 10:32:54 -0500

    complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,9 @@
 /*************************TRIVIAL TRIVIA*******************************/
 /***************PSEUDO CODE*************************/
-
 //MAKE AN ARRAT OF OJECTS
 //EACH OBJECT REPRESENTS A PAGE WITH QUESTIONS AND THE PROSPECTIVE ANSWERS
-
 //CREATE A FUNCTION THAT PRINTS THE OJECTS TO THE THE PAGE, WITH EACH PROPERTY PRIINTED TO ITS RESPECTIVE SPOT ON THE PAGE
 //THERE SHOULD BE A VARIABLE THAT SPECIFIES THE OBJECT IS BEING PRINTED
-
-
 //-------------AND ARRAY OF OBJECTS CONTAINING STRINGS (THE QUESTIONS, CHOICES, AND ANSWERS)-----------
 var items = [
     {
@@ -34,33 +30,29 @@ var items = [
 
     {
         "question": "The very first James Bond film titled Dr. No was filmed in the Caribbean Island of... ",
-        "choices": ["Trinidad & Tobago", "Jamacia", "St. Martin", "Guadeloupe"],
-        "rightAnswer": "Hawaii"
+        "choices": ["Trinidad & Tobago", "Jamaica", "St. Martin", "Guadeloupe"],
+        "rightAnswer": "Jamaica"
     },
 
     {
         "question": "How many countries are there in the Caribbean?",
         "choices": ["100", "50", "75", "25"],
-        "rightAnswer": "Hawaii"
+        "rightAnswer": "75"
     }
 ];
-
 var count = -1,
-    firstCountDown,
-    secondCountDown,
+    firstCountDown, 
+    secondCountDown, 
     answeredCorrectly = 0,
     answeredWrongly = 0,
-    firstTimer,
-    secondTimer;
-
-//----------FUNCTION TO PRINT CONTENT ON THE PAGE--------------
+    unanswered = 0,
+    firstTimer, secondTimer;
+//----------FUNCTION TO PRINT TRIVIA CONTENT ON THE PAGE--------------
 function contentSetter() {
     //print question 
     "use strict";
     $("#question").text((count + 1) + ". " + items[count].question);
-
     //print choices
-
     $("#ans1").html("<span class='items-identifier'>A.</span> " + items[count].choices[0]);
     $("#ans2").html("<span class='items-identifier'>B.</span> " + items[count].choices[1]);
     $("#ans3").html("<span class='items-identifier'>C.</span> " + items[count].choices[2]);
@@ -68,111 +60,149 @@ function contentSetter() {
     $(".items-identifier").css({
         fontWeight: "bold"
     });
-
-
     //print Correct answer 
     $("#answer").html("<span id='items-identifier'>THE ANSWER IS: </span>" + items[count].rightAnswer);
-
     //set value of respective radio buttons
-
     $("#selection-btn1").val(items[count].choices[0]);
     $("#selection-btn2").val(items[count].choices[1]);
     $("#selection-btn3").val(items[count].choices[2]);
     $("#selection-btn4").val(items[count].choices[3]);
 }
-
 //------------------CODE FOR START BUTTON----------------------
-
-$("#start-button").click(function () {
-
-    //change content displayed
-    $("#firstTimer").show();
-    $("#start-page").hide();
-    quesAndAns();
-})
-
-//-----------CODE FOR SUMBIT BUTTON-------------
-
+var strtBtn = $("#start-button").click(function () {
+        //change content displayed
+        $("#firstTimer").show();
+        $("#start-page").hide();
+        $("#timer").show();
+        // load first question using the questionAndAns function 
+        quesAndAns();
+    })
+    //-----------CODE FOR SUMBIT BUTTON-------------
 $("#submit-btn").click(function () {
-    console.log("*******submit btn pressed****");
-    //    firstCountDown = 0;
     $(this).data('clicked', true);
 });
-
-
-
-//------------SETTINGS FOR NEXT BUTTON------------------------
-
-$("#nxt-btn").click(function () {
-    console.log("*******NEXT QUESTION btn pressed****");
-    $(this).data('clicked', true);
-    quesAndAns()
-});
-
-
-//-------CODE FOR FUNCTION THAT PRINTS TRIVIA CONTENT----------
 
 function quesAndAns() {
     "use strict";
-    console.log("---------quesAndAns functions executed------");
     //change content dislayed
+    //this next line is only neccessary when the first question is passed since it the answer display page is... 
+    // ....hidden by default 
     $("#answer-display-page").hide();
+    $("#tick-and-ex").empty();
     $("#trivia-content").show();
-    count++;
-    contentSetter();
-    console.log("count: " + count);
+    count++; //iterates through the item array so that the different trivia items can be loaded 
+    contentSetter(); //loads the triva content unto the page
     firstCountDown = 15;
-
     clearInterval(firstTimer);
+    //this is a countdown to that provides a 15 second window in which the question can be tackled
     firstTimer = setInterval(function () {
         firstCountDown--;
-        console.log("firstCountDown :" + firstCountDown + " & secondCountDown: " + secondCountDown);
         $("#countdown").text(firstCountDown);
+        if (firstCountDown < 11) {
+            $("#countdown").css({
+                color: "darkgreen",
+                textShadow: "2px 2px 4px darkgreen"
+            })
+        }
+        if (firstCountDown < 6) {
+            $("#countdown").css({
+                color: "red",
+                textShadow: "2px 2px 4px red"
+            })
+        }
         if (firstCountDown < 1 || $("#submit-btn").data('clicked')) {
             secondCountDown = 10;
             clearInterval(firstTimer);
-            console.log("------------quesAndAns firstTimer alegedly cleared cause  submitBtn clicked------");
             $("#submit-btn").data('clicked', false);
             answerDisplay();
+            $("#countdown").css({
+                color: "black",
+                textShadow: "2px 2px 4px black"
+            })
         }
     }, 1000);
 }
-
+//------------SETTINGS FOR NEXT BUTTON------------------------
+/*the issue is the quesAndAns function is called twice: once in the secondtimer*/
+$("#nxt-btn").click(function () {
+    $(this).data('clicked', true);
+});
+//-------CODE FOR FUNCTION THAT PRINTS TRIVIA CONTENT----------
 //-------CODE FOR FUNCTION THAT DISPLAYS ANSWER-------------
-
 function answerDisplay() {
     "use strict";
-    console.log("---------answerDisplay functions executed------");
     //change content displayed
-
     $("#trivia-content").hide();
     $("#answer-display-page").show();
     var btnChecked = $("input[type=radio]:checked").val();
     var correctAns = items[count].rightAnswer;
     if (btnChecked === correctAns) {
         answeredCorrectly++;
-        console.log("You Are Corret");
-    } else {
+        $("#correct-or-wrong").html("Contratulations! You are correct.");
+        $("#tick-and-ex").html("<img src='assets/images/tick.png'>");
+    } else if (btnChecked === undefined) {
+        unanswered += 1
+        $("#correct-or-wrong").html("You did not answer the question.");
+    } else if (btnChecked !== correctAns) {
         answeredWrongly++;
-        console.log("You Are Wrong");
+        $("#correct-or-wrong").html("Sorry! You are incorrect.");
+        $("#tick-and-ex").html("<img src='assets/images/x.png'>");
     }
     $("input[type=radio]").prop('checked', false);
-
     clearInterval(secondTimer);
     secondCountDown = 10;
     secondTimer = setInterval(function () {
         secondCountDown--;
         $("#countdown").text(secondCountDown);
-        console.log("Time Left :" + secondCountDown + " $ firstCountDown: " + firstCountDown);
         if (secondCountDown < 1 || $("#nxt-btn").data('clicked')) {
             // Timer stops if time runs out or if sumbit is pressed
             $("#nxt-btn").data('clicked', false);
             clearInterval(secondTimer);
-            console.log("------------secondTimer alegedly cleared----------");
+            if (count === items.length - 1) {
+                clearInterval(secondTimer);
+                return finalPage();
+            }
             secondTimer = 10
             firstCountDown = 15;
-            console.log("...and secondTimer: " + secondTimer);
             quesAndAns();
         }
     }, 1000);
+}
+
+var container = $(".container");
+var rightly = $("<div id='rightly' class='final-page'>");
+var wrong = $("<div id='wrong' class='final-page'>");
+var unans = $("<div id='unans' class='final-page'>");
+var resetBtn = $("<button id='reset-btn' class='final-page'>Reset</button>");
+
+resetBtn.click(function () {
+    count = -1;
+    answeredCorrectly = 0;
+    answeredWrongly  = 0;
+    $(this).hide();
+    $("#answer-display-page").hide();
+    $("#final-page").show();
+    $("#trivia-content").hide();
+    $("#timer").hide();
+    $(".final-page").hide();
+    $("#start-page").show();
+});
+
+resetBtn.addClass("btn");
+
+function finalPage() {
+    $(".final-page").show();
+    $("#answer-display-page").hide();
+    $("#final-page").show();
+    $("#trivia-content").hide();
+    $("#timer").hide();
+    rightly.text("Questions Answered Correctly: " + answeredCorrectly);
+    wrong.text("Questions Answered Incorrectly: " + answeredWrongly);
+    if (unanswered > 0 ) {
+    unans.text("YOU LEFT " + unanswered + " QUESTIONS UNANSWERED.");
+    }
+    container.append(rightly);
+    container.append(wrong);
+    container.append(unans);
+    container.append(resetBtn);
 }
